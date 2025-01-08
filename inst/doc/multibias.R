d5b2e535a1be44ffedacb3f95b0b946ef90c5952
Paul Brendel <pcbrendel@gmail.com>
2025-01-08 19:20:05 +0000

    version 1.6.2

--- inst/doc/multibias.R
+++ inst/doc/multibias.R
@@ -81,61 +81,61 @@ s_model <- glm(S ~ Xstar + Y + C1 + C2 + C3,
                data = df_uc_em_sel_source)
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  library(doParallel)
-#  
-#  no_cores <- detectCores() - 1
-#  registerDoParallel(cores = no_cores)
-#  cl <- makeCluster(no_cores)
-#  
-#  set.seed(1234)
-#  nreps <- 1000
-#  est <- vector(length = nreps)
+# library(doParallel)
+# 
+# no_cores <- detectCores() - 1
+# registerDoParallel(cores = no_cores)
+# cl <- makeCluster(no_cores)
+# 
+# set.seed(1234)
+# nreps <- 1000
+# est <- vector(length = nreps)
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  or <- foreach(i = 1:nreps, .combine = c,
-#                .packages = c("dplyr", "multibias")) %dopar% {
-#  
-#    df_sample <- df_uc_em_sel[sample(seq_len(nrow(df_uc_em_sel)),
-#                                     nrow(df_uc_em_sel),
-#                                     replace = TRUE), ]
-#  
-#    est[i] <- adjust_uc_em_sel(
-#      data_observed = data_observed(
-#        data = df_sample,
-#        exposure = "Xstar",
-#        outcome = "Y",
-#        confounders = c("C1", "C2", "C3")
-#      ),
-#      u_model_coefs = c(
-#        rnorm(1, mean = u_model$coef[1], sd = summary(u_model)$coef[1, 2]),
-#        rnorm(1, mean = u_model$coef[2], sd = summary(u_model)$coef[2, 2]),
-#        rnorm(1, mean = u_model$coef[3], sd = summary(u_model)$coef[3, 2])
-#      ),
-#      x_model_coefs = c(
-#        rnorm(1, mean = x_model$coef[1], sd = summary(x_model)$coef[1, 2]),
-#        rnorm(1, mean = x_model$coef[2], sd = summary(x_model)$coef[2, 2]),
-#        rnorm(1, mean = x_model$coef[3], sd = summary(x_model)$coef[3, 2]),
-#        rnorm(1, mean = x_model$coef[4], sd = summary(x_model)$coef[4, 2]),
-#        rnorm(1, mean = x_model$coef[5], sd = summary(x_model)$coef[5, 2]),
-#        rnorm(1, mean = x_model$coef[6], sd = summary(x_model)$coef[6, 2])
-#      ),
-#      s_model_coefs = c(
-#        rnorm(1, mean = s_model$coef[1], sd = summary(s_model)$coef[1, 2]),
-#        rnorm(1, mean = s_model$coef[2], sd = summary(s_model)$coef[2, 2]),
-#        rnorm(1, mean = s_model$coef[3], sd = summary(s_model)$coef[3, 2]),
-#        rnorm(1, mean = s_model$coef[4], sd = summary(s_model)$coef[4, 2]),
-#        rnorm(1, mean = s_model$coef[5], sd = summary(s_model)$coef[5, 2]),
-#        rnorm(1, mean = s_model$coef[6], sd = summary(s_model)$coef[6, 2])
-#      )
-#    )$estimate
-#  }
+# or <- foreach(i = 1:nreps, .combine = c,
+#               .packages = c("dplyr", "multibias")) %dopar% {
+# 
+#   df_sample <- df_uc_em_sel[sample(seq_len(nrow(df_uc_em_sel)),
+#                                    nrow(df_uc_em_sel),
+#                                    replace = TRUE), ]
+# 
+#   est[i] <- adjust_uc_em_sel(
+#     data_observed = data_observed(
+#       data = df_sample,
+#       exposure = "Xstar",
+#       outcome = "Y",
+#       confounders = c("C1", "C2", "C3")
+#     ),
+#     u_model_coefs = c(
+#       rnorm(1, mean = u_model$coef[1], sd = summary(u_model)$coef[1, 2]),
+#       rnorm(1, mean = u_model$coef[2], sd = summary(u_model)$coef[2, 2]),
+#       rnorm(1, mean = u_model$coef[3], sd = summary(u_model)$coef[3, 2])
+#     ),
+#     x_model_coefs = c(
+#       rnorm(1, mean = x_model$coef[1], sd = summary(x_model)$coef[1, 2]),
+#       rnorm(1, mean = x_model$coef[2], sd = summary(x_model)$coef[2, 2]),
+#       rnorm(1, mean = x_model$coef[3], sd = summary(x_model)$coef[3, 2]),
+#       rnorm(1, mean = x_model$coef[4], sd = summary(x_model)$coef[4, 2]),
+#       rnorm(1, mean = x_model$coef[5], sd = summary(x_model)$coef[5, 2]),
+#       rnorm(1, mean = x_model$coef[6], sd = summary(x_model)$coef[6, 2])
+#     ),
+#     s_model_coefs = c(
+#       rnorm(1, mean = s_model$coef[1], sd = summary(s_model)$coef[1, 2]),
+#       rnorm(1, mean = s_model$coef[2], sd = summary(s_model)$coef[2, 2]),
+#       rnorm(1, mean = s_model$coef[3], sd = summary(s_model)$coef[3, 2]),
+#       rnorm(1, mean = s_model$coef[4], sd = summary(s_model)$coef[4, 2]),
+#       rnorm(1, mean = s_model$coef[5], sd = summary(s_model)$coef[5, 2]),
+#       rnorm(1, mean = s_model$coef[6], sd = summary(s_model)$coef[6, 2])
+#     )
+#   )$estimate
+# }
 
 ## ----eval = FALSE-------------------------------------------------------------
-#  # odds ratio estimate
-#  round(median(or), 2)
-#  #> 2.02
-#  
-#  # confidence interval
-#  round(quantile(or, c(.025, .975)), 2)
-#  #> 1.93 2.11
+# # odds ratio estimate
+# round(median(or), 2)
+# #> 2.02
+# 
+# # confidence interval
+# round(quantile(or, c(.025, .975)), 2)
+# #> 1.93 2.11
 
